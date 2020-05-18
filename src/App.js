86991bf3073b2e60d83b0455b5dfb96d34cb84a5
Diego De Granda <degranda@gmail.com>
2020-05-18 18:28:19 -0500

    Adding redux to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -7,40 +8,43 @@ import SearchBox from './components/SearchBox';
 import Scroll from './components/Scroll';
 import ErrorBoundry from './components/ErrorBoundry';
 
-import { robots } from './robots';
+import { setSearchField, requestRobots } from './actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRoots: () => dispatch(requestRobots())
+  }
+}
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state =  {
-      robots: [],
-      searchfield: ''
-    }
-  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }))
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+    this.props.onRequestRoots();
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
     });
 
-    if(this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    } else {
-      return (
+    return isPending ?
+     <h1>Loading...</h1> :
+      (
         <div>
           <header>
             <h1>RoboFriends</h1>  
-            <SearchBox searchChange={this.onSearchChange}/>
+            <SearchBox searchChange={onSearchChange}/>
           </header>
           <main>
             <Scroll>
@@ -51,8 +55,7 @@ class App extends React.Component {
           </main>
         </div>
       )
-    }
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
