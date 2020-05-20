0314d565d2ccb3cce98675f8a81243ae09214bfb
Diego De Granda <degranda@gmail.com>
2020-05-20 17:20:26 -0500

    Updates

---
+++ precache-manifest.0810b21d08b48ab7fe3e1370fafd93a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ab1c5fd062df0d7c7cc7ba7859c5ee",
+    "url": "/Robot-app/index.html"
+  },
+  {
+    "revision": "b3da02228b510a7275b1",
+    "url": "/Robot-app/static/css/main.e5a5be4d.chunk.css"
+  },
+  {
+    "revision": "d1d3fc27e122a526cf7b",
+    "url": "/Robot-app/static/js/2.3ec213ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Robot-app/static/js/2.3ec213ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3da02228b510a7275b1",
+    "url": "/Robot-app/static/js/main.309f3ce6.chunk.js"
+  },
+  {
+    "revision": "5ef801063f04084411d8",
+    "url": "/Robot-app/static/js/runtime-main.4fc8150c.js"
+  }
+]);
