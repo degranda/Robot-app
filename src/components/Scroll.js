4bd51c434e497872c819ffe7b28ac06f0d9079a0
Diego De Granda <degranda@gmail.com>
2020-05-29 11:45:28 -0500

    fixing grid layout

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-        <div style={{ overflowY: 'scroll', height: '90vh' }}>
+        <div style={{ overflowY: 'scroll', height: '90vh', padding: '0 10px' }}>
             {props.children}
         </div>
     ); 
